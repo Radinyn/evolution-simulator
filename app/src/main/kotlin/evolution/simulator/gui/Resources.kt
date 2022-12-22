978ac6c4c0198ea11eaaf525357c5c589275b408
Bartosz Nowak <bartekn337@gmail.com>
2022-12-22 11:11:20 +0100

    simulation thread

--- app/src/main/kotlin/evolution/simulator/gui/Resources.kt
+++ app/src/main/kotlin/evolution/simulator/gui/Resources.kt
@@ -5,10 +5,10 @@ import java.io.FileInputStream
 
 class Resources {
     val plantImage = Image(FileInputStream("src/main/resources/plant.png"))
-    val plantImageWidth = 40.0
-    val plantImageHeight = 40.0
+    val plantImageWidth = 30.0
+    val plantImageHeight = 30.0
 
     val animalImage = Image(FileInputStream("src/main/resources/animal.png"))
-    val animalImageWidth = 40.0
-    val animalImageHeight = 40.0
+    val animalImageWidth = 30.0
+    val animalImageHeight = 30.0
 }
--- app/src/main/kotlin/evolution/simulator/gui/SimulationWindow.kt
+++ app/src/main/kotlin/evolution/simulator/gui/SimulationWindow.kt
@@ -1,79 +1,70 @@
 package evolution.simulator.gui
 
-import javafx.stage.Stage
-import javafx.scene.Scene
-
-import javafx.scene.layout.*
-import javafx.scene.control.*
-
 import evolution.simulator.*
+import evolution.simulator.Map
+import javafx.event.EventHandler
+import javafx.scene.Scene
+import javafx.scene.control.ToggleButton
+import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
+import javafx.stage.Stage
+import kotlin.random.Random
 
 class SimulationWindow(private val params: SimulationParameters, private val stage: Stage) {
 
     fun start() {
-        // val chart = Chart()
-        // chart.addPoint(0, 0)
-        // chart.addPoint(20, 14)
-        // chart.addPoint(60, 43)
-        // chart.addPoint(80, 32)
-        // chart.addPoint(100, 43)
-
         val charts = StatisticsCharts()
-
-        // val strategy = Strategy(SimulationParameters(
-        //     animalBehavior = AnimalBehavior.STRICT,
-        //     genomeLength = 10,
-        //     initAnimalEnergy = 10,
-        //     initAnimalNum = 10,
-        //     initPlantsNum = 10,
-        //     mapType = MapType.GLOBE,
-        //     mutationMaxNum = 10,
-        //     mutationMinNum = 0,
-        //     mutationType = MutationType.RANDOM,
-        //     plantEnergy = 10,
-        //     plantGrowthRate = 100,
-        //     plantGrowthType = GrowthType.EQUATOR,
-        //     reproductionCost = 10,
-        //     width = 30,
-        //     height = 30,
-        //     stuffedThreshold = 5,
-        // ))
-
         val strategy = Strategy(params)
         val map = Map(strategy, Resources())
         val grid = Grid(params.width, params.height, map)
 
-        map.plantGrowthPhase()
-        map.place(Animal(Vector2d(3, 3), Orientation.NORTH_EAST, params.initAnimalEnergy, 0, 0, 0, Genome(strategy, ArrayList()), strategy))
-        grid.update()
+        repeat(params.initAnimalNum) {
+            map.place(Animal(
+                Vector2d(Random.nextInt(params.width), Random.nextInt(params.width)),
+                Orientation.NORTH_EAST, params.initAnimalEnergy,
+                0, 0, 0,
+                Genome.generateRandom(strategy), strategy)
+            )
+        }
 
-
-        val s1 = Statistics(0u, 2u, 1u, 1u, Genome(strategy, ArrayList()), 69.0, 420.0)
-        val s2 = Statistics(1u, 4u, 7u, 1u, Genome(strategy, ArrayList()), 96.0, 240.0)
-        val s3 = Statistics(2u, 3u, 20u, 3u, Genome(strategy, ArrayList()), 69.0, 42.0)
-
-
-        charts.update(s1)
-        charts.update(s2)
-        charts.update(s3)
-
-
+        // TODO trzeba to wynieśc w klase która implementuje runnable itp żeby można było stopować i resumować wątek wait() i awake() na razie prowizorka
+        val simThreadHandle = Thread {
+            var cnt: ULong = 0u
+            while (true){
+                Thread.sleep(100)
+                map.plantGrowthPhase()
+                map.movePhase()
+                map.agePhase()
+                map.eatingPhase()
+                map.matingPhase()
+                map.deathPhase(cnt)
+                grid.update()
+                val stats = map.statistics()
+                stats.numOfIterations = cnt
+                charts.update(stats)
+                cnt += 1u
+            }
+        }
         
         val hbox = HBox()
         hbox.children.addAll(grid.asNode(), charts.asNode())
-
 
         val hbox2 = HBox()
         val playButton = ToggleButton("Play")
         val recordButton = ToggleButton("Record")
         hbox2.children.addAll(playButton, recordButton)
 
-        val vbox = VBox(hbox, hbox2)
+        playButton.onAction = EventHandler {
 
+        }
+
+        val vbox = VBox(hbox, hbox2)
         val scene = Scene(vbox, grid.width*40.0 + 400, grid.height*40.0 + 20)
 
         stage.title = "Evolution Simulator"
         stage.scene = scene
         stage.show()
+
+        simThreadHandle.start()
     }
 }
